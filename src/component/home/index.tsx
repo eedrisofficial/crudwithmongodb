5477232487fdb7cc747cca6e2074b4731668ea83
eedrisofficial <eedrisofficial.gmail.com>
2024-04-17 08:14:47 -0400

    first commit

---
+++ src/component/home/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Index = () => {
+  return <div>Hello users</div>;
+};
+
+export default Index;
---
+++ src/utils/const/user.ts
@@ -0,0 +1,30 @@
+export const userDetails = [
+  {
+    imgURL:
+      "https://cdn.hashnode.com/res/hashnode/image/upload/v1682995925043/AHRGgD_9F.jpeg?w=500&h=500&fit=crop&crop=faces&auto=compress,format&format=webp",
+    name: "Haruna Idris",
+    brief:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, porro.",
+  },
+  {
+    imgURL:
+      "https://cdn.hashnode.com/res/hashnode/image/upload/v1682995925043/AHRGgD_9F.jpeg?w=500&h=500&fit=crop&crop=faces&auto=compress,format&format=webp",
+    name: "Nice Idris",
+    brief:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, porro.",
+  },
+  {
+    imgURL:
+      "https://cdn.hashnode.com/res/hashnode/image/upload/v1682995925043/AHRGgD_9F.jpeg?w=500&h=500&fit=crop&crop=faces&auto=compress,format&format=webp",
+    name: "CityBoy Idris",
+    brief:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, porro.",
+  },
+  {
+    imgURL:
+      "https://cdn.hashnode.com/res/hashnode/image/upload/v1682995925043/AHRGgD_9F.jpeg?w=500&h=500&fit=crop&crop=faces&auto=compress,format&format=webp",
+    name: "Ibrahim Idris",
+    brief:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, porro.",
+  },
+];
