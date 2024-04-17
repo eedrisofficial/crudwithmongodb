d32465bbeb7bd86f053db18b2a54bdc41fa9c8d0
eedrisofficial <eedrisofficial.gmail.com>
2024-04-17 08:18:29 -0400

    id added to users

--- src/utils/const/user.ts
+++ src/utils/const/user.ts
@@ -1,5 +1,6 @@
 export const userDetails = [
   {
+    id: 1,
     imgURL:
       "https://cdn.hashnode.com/res/hashnode/image/upload/v1682995925043/AHRGgD_9F.jpeg?w=500&h=500&fit=crop&crop=faces&auto=compress,format&format=webp",
     name: "Haruna Idris",
@@ -7,6 +8,7 @@ export const userDetails = [
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, porro.",
   },
   {
+    id: 2,
     imgURL:
       "https://cdn.hashnode.com/res/hashnode/image/upload/v1682995925043/AHRGgD_9F.jpeg?w=500&h=500&fit=crop&crop=faces&auto=compress,format&format=webp",
     name: "Nice Idris",
@@ -14,6 +16,7 @@ export const userDetails = [
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, porro.",
   },
   {
+    id: 3,
     imgURL:
       "https://cdn.hashnode.com/res/hashnode/image/upload/v1682995925043/AHRGgD_9F.jpeg?w=500&h=500&fit=crop&crop=faces&auto=compress,format&format=webp",
     name: "CityBoy Idris",
@@ -21,6 +24,7 @@ export const userDetails = [
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere, porro.",
   },
   {
+    id: 4,
     imgURL:
       "https://cdn.hashnode.com/res/hashnode/image/upload/v1682995925043/AHRGgD_9F.jpeg?w=500&h=500&fit=crop&crop=faces&auto=compress,format&format=webp",
     name: "Ibrahim Idris",
